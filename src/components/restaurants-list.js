adeaf340ec0273d188079ded6071faf126fb9ac4
Yego Allan <allanyego05@gmail.com>
2022-07-22 06:18:57 +0000

    Developed the Searchbar on restaurants list

--- src/components/restaurants-list.js
+++ src/components/restaurants-list.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 import restaurantService from "../services/restaurants";
 
@@ -17,10 +18,10 @@ export default function RestaurantsList() {
   const getRestaurants = () => {
     restaurantService
       .getAll()
-      .then(resp => {
+      .then((resp) => {
         setRestaurants(resp.data.restaurants);
       })
-      .catch(e => {
+      .catch((e) => {
         console.log(`Error, ${e}`);
       });
   };
@@ -28,37 +29,37 @@ export default function RestaurantsList() {
   const getCuisines = () => {
     restaurantService
       .getCuisines()
-      .then(resp => {
+      .then((resp) => {
         setCuisines([...cuisines, ...resp.data]);
       })
-      .catch(e => {
+      .catch((e) => {
         console.log(`Error, ${e}`);
       });
   };
 
-  const onNameChange = e => {
+  const onNameChange = (e) => {
     setSearchName(e.target.value);
   };
 
-  const onZiphange = e => {
+  const onZiphange = (e) => {
     setSearchZip(e.target.value);
   };
 
-  const onCuisineChange = e => {
+  const onCuisineChange = (e) => {
     setSearchCuisine(e.target.value);
   };
 
   const find = (query, by) => {
     restaurantService
       .find(query, by)
-      .then(resp => {
+      .then((resp) => {
         setRestaurants(resp.data.restaurants);
       })
-      .catch(e => console.log(`Error, ${e}`));
+      .catch((e) => console.log(`Error, ${e}`));
   };
 
   const refreshList = () => {
-      getRestaurants();
+    getRestaurants();
   };
 
   const findByName = () => {
@@ -79,24 +80,108 @@ export default function RestaurantsList() {
 
   return (
     <div>
+      {/* Search bar */}
       <div className="row pb-1">
-        <div className="input-group col-lg-4">
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Recipient's username"
-            aria-label="Recipient's username"
-            aria-describedby="button-addon2"
-          />
-          <button
-            className="btn btn-outline-secondary"
-            type="button"
-            id="button-addon2"
-          >
-            Button
-          </button>
+        <div className="col-4">
+          <div className="input-group">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Search by name"
+              value={searchName}
+              onChange={onNameChange}
+            />
+            <button
+              className="btn btn-outline-secondary"
+              type="button"
+              onClick={findByName}
+            >
+              Search
+            </button>
+          </div>
+        </div>
+
+        <div className="col-4">
+          <div className="input-group">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Search by name"
+              value={searchZip}
+              onChange={onZiphange}
+            />
+            <button
+              className="btn btn-outline-secondary"
+              type="button"
+              onClick={findByZip}
+            >
+              Search
+            </button>
+          </div>
+        </div>
+
+        <div className="col-4">
+          <div className="input-group">
+            <select onChange={onCuisineChange}>
+              {cuisines.map((cuisine) => (
+                <option value={cuisine} key={cuisine}>
+                  {cuisine.substring(0, 20)}
+                </option>
+              ))}
+            </select>
+            <button
+              className="btn btn-outline-secondary"
+              type="button"
+              onClick={findByCuisine}
+            >
+              Search
+            </button>
+          </div>
         </div>
       </div>
+      {/* End of searchbar */}
+
+      {/* Restaurant list */}
+      <div className="row">
+        {restaurants.map((restaurant) => {
+          const { building, street, zipcode } = restaurant.address;
+          const address = `${building} ${street} ${zipcode}`;
+
+          return (
+            <div className="col-lg-4 pb-1">
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="cart-title">{restaurant.name}</h5>
+                  <p className="card-text">
+                    <strong>Cuisine: </strong>
+                    {restaurant.cuisine}
+                    <br />
+                    <strong>Address: </strong>
+                    {address}
+                  </p>
+
+                  <div className="row">
+                    <Link
+                      to={`/restaurants/${restaurant._id}`}
+                      className="btn btn-primary col-lg-5 mx-1 mb-1"
+                    >
+                      View Reviews
+                    </Link>
+                    <a
+                      target="_blank"
+                      href={"https://www.google.com/maps/place/" + address}
+                      className="btn btn-primary col-lg-5 mx-1 mb-1"
+                    >
+                      View Map
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      {/* End of restaurant list */}
     </div>
   );
 }
--- src/services/http-common.js
+++ src/services/http-common.js
@@ -1,8 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
 
-export default axios.creat({
-    baseURL: 'http://localhost:5000/api/v1/restaurants',
-    headers: {
-        'Content-Type': 'application/json'
-    }
+export default axios.create({
+  //   baseURL: "http://localhost:5000/api/v1/restaurants",
+  baseURL:
+    "https://5000-yegow-doniceserver-wqa7m78obof.ws-eu54.gitpod.io/api/v1/restaurants",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
